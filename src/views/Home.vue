21688d91ee47431a014e0d64f637f203617a22fb
koonlucky555 <koonlucky123@gmail.com>
2021-04-20 00:43:56 +0700

    [CREATE] Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home grid grid-cols-3 h-screen w-full">
+    <div class="bg-color space-y-8">
+      <div class="flex justify-center">
+        <img src="../assets/logo.png " class="col-start-2 pt-20" />
+      </div>
+
+      <div class="space-y-6">
+        <p class="font text-2xl Asul-font">Shabu & Yakiniku</p>
+        <p class="font text-6xl Asul-font">Eat Else</p>
+      </div>
+
+      <div>
+        <a
+          href="/"
+          class="click uppercase rounded-md  text-white px-4 py-3 transition 
+          duration-300 ease-in-out hover:bg-white hover:bg-opacity-60 hover:text-white mr-6 Asul-font"
+        >
+          Go to site
+        </a>
+      </div>
+    </div>
+
+    <div class="col-span-2">
+      <img src="../assets/background.png " />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scope>
+.bg-color {
+  background-color: #434343;
 }
+.font {
+  color: #c4c4c4;
+}
+.click{
+  background-color:#dfad2f;
+}
+.Asul-font{
+  font-family: "Asul-font";
+}
+@font-face {
+    font-family: "Asul-font";
+    src: url("../font/Asul-Regular.ttf");
+}
+</style>
+<script>
+export default {};
 </script>
